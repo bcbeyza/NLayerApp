2a4facb178cdf715c378b3576406f317552bce2a
Beyza Nur Çetiner <hcbeyzanur7@gmail.com>
2023-03-16 15:53:52 +0300

    Add project files.

---
+++ NLayer.Repository/Seeds/ProductSeed.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Seeds
+{
+    public class ProductSeed : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasData(new Product
+            {
+                Id=1,
+                Name="Kalem 1",
+                CategoryId=1,
+                Price=100,
+                Stock=20,
+                CreateDate=DateTime.Now,
+
+            },
+            new Product 
+            {
+                Id = 2,
+                Name="Kalem 2",
+                CategoryId = 1,
+                Price = 100,
+                Stock = 20,
+                CreateDate = DateTime.Now,
+
+            },
+
+            new Product
+            {
+                Id = 3,
+                Name = "Kalem 3",
+                CategoryId = 1,
+                Price = 100,
+                Stock = 20,
+                CreateDate = DateTime.Now,
+            },
+
+             new Product
+             {
+                 Id = 4,
+                 Name = "Kitap 1",
+                 CategoryId = 2,
+                 Price = 100,
+                 Stock = 20,
+                 CreateDate = DateTime.Now,
+             },
+
+             new Product
+             {
+                 Id = 5,
+                 Name = "Kitap 2",
+                 CategoryId = 2,
+                 Price = 100,
+                 Stock = 20,
+                 CreateDate = DateTime.Now,
+             }
+
+
+            );
+        }
+    }
+}
